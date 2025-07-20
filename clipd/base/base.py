bcc960055bc5b37ffa7de0d813e8e991d9ff5abd
Yadh <yadhnikawakde@gmail.com>
2025-07-20 22:15:37 +0530

    fixed decorator bug

--- clipd/base/base.py
+++ clipd/base/base.py
@@ -2,6 +2,7 @@ from clipd.base.init import Init
 from clipd.base.connect import Connect
 from clipd.base.describe import Describe
 from clipd.base.log import log_app
+from clipd.base.test import Test
 from clipd.base.eggs import Thankyou, Why, SelfDestruct
 from clipd.base.export import Export
 from clipd.base.disconnect import Disconnect
@@ -17,6 +18,8 @@ def base():
     app.command("describe", help = "Pandas describe function. ")(Describe.describe)
     app.command("export", help = "Export the modified DataFrame to the current directory in csv.")(Export.export)
     app.command("disconnect", help = "Disconnects from active file")(Disconnect.disconnect)
+    app.command("test")(Test.test)
+
     app.command("thankyou", help = "Gratitude message")(Thankyou.thank_you)
     app.command("why", help = "The origin of clipd")(Why.why)
     app.command("selfdestruct")(SelfDestruct.self_destruct)
